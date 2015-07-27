d024fbc5b9400f4bc812aa815cd9cd7eb0c86dfd
Philippe Vinchon <p.vinchon@gmail.com>
2015-07-27 21:13:12 +0100

    Implement abstraction

--- src/extends-ng-model/ng-model-location.js
+++ src/extends-ng-model/ng-model-location.js
@@ -1,27 +1,8 @@
-angular.module('extendsNgModel').directive('ngModelLocation', function($location, ngModelConverter) {
-  var link = function(scope, element, attributes, ngModelCtrl) {
-    var inputType = (attributes.type || 'text'),
-      searchKey = attributes.ngModelLocation || attributes.ngModel,
-      searchValue = function() { return $location.search()[searchKey]; };
-
-    var updateSearchValue = function(ngModelValue) {
-      var value = ngModelConverter(ngModelValue, inputType);
-
-      $location.search(searchKey, value);
-      return ngModelValue;
-    }
-
-    var updateModelValue = function(searchValue) {
-      var value = angular.isDefined(searchValue) ? searchValue : ngModelCtrl.$modelValue,
-        updatedValue = ngModelConverter(value, inputType);
-
-      ngModelCtrl.$setViewValue(updatedValue, this);
-      ngModelCtrl.$render();
-    }
-
-    ngModelCtrl.$parsers.push(updateSearchValue);
-    scope.$watch(searchValue, updateModelValue);
-  }
+angular.module('extendsNgModel').directive('ngModelLocation', function($location, ngModelStorage) {
+  var name = "ngModelLocation",
+    getValue = function(key) { return function() { return $location.search()[key]; } },
+    putValue = function(key, value) { $location.search(key, value); },
+    link = ngModelStorage(name, getValue, putValue);
 
   return {
     require: 'ngModel',
